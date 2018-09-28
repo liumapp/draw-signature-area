bbebc2592c6905ee3b5370dc5c5229bd98ca7495
liumapp <liumapp.com@gmail.com>
2018-09-28 17:05:35 +0800

    add upload doc controller

---
+++ draw-service/src/main/java/com/liumapp/demo/draw/signature/service/controller/UploadDocController.java
@@ -0,0 +1,23 @@
+package com.liumapp.demo.draw.signature.service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * file UploadDocController.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/9/28
+ */
+@RequestMapping("upload")
+@RestController
+public class UploadDocController {
+
+    @RequestMapping("/doc")
+    public void uploadDoc () {
+
+    }
+
+}
