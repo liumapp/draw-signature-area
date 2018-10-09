31f77d1f0d3c8d1c0e66c132e14c9d3057d9f848
liumapp <liumapp.com@gmail.com>
2018-10-09 14:39:38 +0800

    draw page could return to converting page

--- draw-ui/src/components/draw-area.vue
+++ draw-ui/src/components/draw-area.vue
@@ -7,11 +7,41 @@
  */
 <template>
 <div>
-  this is draw area page .
+  <br>
+  <Row>
+    <Col span="8" offset="8">
+    <Upload
+      multiple
+      type="drag"
+      action="#"
+      accept=".doc, .docx"
+      :before-upload="handleFileToBase64"
+      :default-file-list="readyUploadFile"
+      :on-remove="handleRmFile">
+      <div style="padding: 20px 0">
+        <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
+        <p>Click or drag files here to upload</p>
+      </div>
+    </Upload>
+    </Col>
+  </Row>
+  <Row>
+    <Col span="2" offset="14">
+    <Button type="default" @click="back">返回</Button>
+    </Col>
+    <Col span="2">
+    <Button type="primary" @click="submitPic">提交</Button>
+    </Col>
+  </Row>
 </div>
 </template>
 <script>
 export default {
-  name: 'drawArea'
+  name: 'drawArea',
+  methods: {
+    back () {
+      this.$emit('prev');
+    }
+  }
 }
 </script>
