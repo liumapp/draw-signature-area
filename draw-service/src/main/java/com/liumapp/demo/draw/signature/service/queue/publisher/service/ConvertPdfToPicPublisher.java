dc189d5e1e1c7e39ab4f50559fc2e8c28081cd09
liumapp <liumapp.com@gmail.com>
2018-10-09 17:30:02 +0800

    update convert pdf to pic publisher

--- draw-service/src/main/java/com/liumapp/demo/draw/signature/service/queue/publisher/service/ConvertPdfToPicPublisher.java
+++ draw-service/src/main/java/com/liumapp/demo/draw/signature/service/queue/publisher/service/ConvertPdfToPicPublisher.java
@@ -1,6 +1,8 @@
 package com.liumapp.demo.draw.signature.service.queue.publisher.service;
 
 import com.liumapp.rabbitmq.publisher.BasicPublisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.support.CorrelationData;
 
 /**
@@ -13,19 +15,28 @@ import org.springframework.amqp.rabbit.support.CorrelationData;
  */
 public class ConvertPdfToPicPublisher extends BasicPublisher {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public void confirm(CorrelationData correlationData, boolean ack, String cause) {
-
+        logger.info("convert pdf to pic service get confirmed info : " + correlationData);
+        if (ack) {
+            logger.info("send msg success");
+        } else {
+            logger.error("send msg failed and the reason is : " + cause);
+        }
     }
 
     @Override
     public void setCallBack() {
-
+        rabbitTemplate.setReturnCallback(this);
+        rabbitTemplate.setConfirmCallback(this);
     }
 
     @Override
     public void send(String msg) {
-
+        logger.info("convert pdf to pic service ready to send msg : " + msg);
+        this.sendMessage("convertPdfToPicConsumer", "process", Integer.toString(10113), msg);
     }
 
 }
