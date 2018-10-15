71e0407bce8bd02d89059db1ad1992890295c226
liumapp <liumapp.com@gmail.com>
2018-10-15 14:39:44 +0800

    add pic page render for tool

---
+++ draw-ui/src/components/tool/PicPageRender.vue
@@ -0,0 +1,17 @@
+/**
+ * @file PicPageRender.vue
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/10/15
+ */
+<template>
+  <div>
+    this is page render
+  </div>
+</template>
+<script>
+export default {
+  name: 'picPageRender'
+}
+</script>
